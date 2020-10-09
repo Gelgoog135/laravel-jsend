225cf49ae668704fb3f857b41501127211c1ffe2
Gelgoog135 <44486808+Gelgoog135@users.noreply.github.com>
2020-10-10 02:26:44 +0800

    Update helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -2,6 +2,7 @@
 if(!class_exists('QueryType')) {
     abstract class QueryType
     {
+        const Undefined = -1;
         const Create = 1;
         const Read = 2;
         const Update = 3;
@@ -33,45 +34,33 @@ if (!function_exists("jsend_error")) {
 
 if (!function_exists("jsend_fail")) {
     /**
+     * @param QueryType $type Query Type
      * @param array $data
      * @param int $status HTTP status code
      * @param array $extraHeaders
      * @return \Illuminate\Contracts\Routing\ResponseFactory|\Symfony\Component\HttpFoundation\Response
      */
-    function jsend_fail($data, $status = 400, $extraHeaders = [])
+    function jsend_fail($type = QueryType::Undefined, $data = null, $status = 400, $extraHeaders = [])
     {
-        $response = [
-            "status" => "fail",
-            "data" => $data
-        ];
-
-        return response()->json($response, $status, $extraHeaders);
-    }
-    
-    /**
-     * @param QueryType $type Query Type
-     * @param int $status HTTP status code
-     * @param array $extraHeaders
-     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Symfony\Component\HttpFoundation\Response
-     */
-    function jsend_success(QueryType $type, $status = 400, $extraHeaders = [])
-    {
-        switch($type) {
-            case QueryType::Create:
-                $data = ["message" => "Created Failed"];
-                break;
-            case QueryType::Read:
-                $data = ["message" => "Read Failed"];
-                break;
-            case QueryType::Update:
-                $data = ["message" => "Updated Failed"];
-                break;
-            case QueryType::Delete:
-                $data = ["message" => "Deleted Failed"];
-                break;
-            default:
-                $data = null;
+        if($type != QueryType::Undefined){
+            switch($type) {
+                case QueryType::Create:
+                    $data = ["message" => "Created Failed"];
+                    break;
+                case QueryType::Read:
+                    $data = ["message" => "Read Failed"];
+                    break;
+                case QueryType::Update:
+                    $data = ["message" => "Updated Failed"];
+                    break;
+                case QueryType::Delete:
+                    $data = ["message" => "Deleted Failed"];
+                    break;
+                default:
+                    $data = null;
+            }
         }
+        
         $response = [
             "status" => "fail",
             "data" => $data
@@ -83,45 +72,33 @@ if (!function_exists("jsend_fail")) {
 
 if (!function_exists("jsend_success")) {
     /**
+     * @param QueryType $type Query Type
      * @param array | Illuminate\Database\Eloquent\Model $data
      * @param int $status HTTP status code
      * @param array $extraHeaders
      * @return \Illuminate\Contracts\Routing\ResponseFactory|\Symfony\Component\HttpFoundation\Response
      */
-    function jsend_success($data = [], $status = 200, $extraHeaders = [])
+    function jsend_success($type = QueryType::Undefined, $data = [], $status = 200, $extraHeaders = [])
     {
-        $response = [
-            "status" => "success",
-            "data" => $data
-        ];
-
-        return response()->json($response, $status, $extraHeaders);
-    }
-    
-    /**
-     * @param QueryType $type Query Type
-     * @param int $status HTTP status code
-     * @param array $extraHeaders
-     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Symfony\Component\HttpFoundation\Response
-     */
-    function jsend_success(QueryType $type, $status = 200, $extraHeaders = [])
-    {
-        switch($type) {
-            case QueryType::Create:
-                $data = ["message" => "Created Successfully"];
-                break;
-            case QueryType::Read:
-                $data = ["message" => "Created Successfully"];
-                break;
-            case QueryType::Update:
-                $data = ["message" => "Updated Successfully"];
-                break;
-            case QueryType::Delete:
-                $data = ["message" => "Deleted Successfully"];
-                break;
-            default:
-                $data = null;
+        if($type != QueryType::Undefined){
+            switch($type) {
+                case QueryType::Create:
+                    $data = ["message" => "Created Successfully"];
+                    break;
+                case QueryType::Read:
+                    $data = ["message" => "Read Successfully"];
+                    break;
+                case QueryType::Update:
+                    $data = ["message" => "Updated Successfully"];
+                    break;
+                case QueryType::Delete:
+                    $data = ["message" => "Deleted Successfully"];
+                    break;
+                default:
+                    $data = null;
+            }
         }
+        
         $response = [
             "status" => "success",
             "data" => $data
